0bc6b1ae9781bdfbb0b09961b913abf6da16d106
Pratyush Raj <60818670+Pratyush017@users.noreply.github.com>
2022-02-28 14:09:32 +0530

    Update watch.py

--- watch.py
+++ watch.py
@@ -2,6 +2,6 @@ import pyautogui as pg
 import time 
 time.sleep(10)
 
-for i in range(1000):
-    pg.write('<@861552831671304232>')
+for i in range("spam amount in numbers without quotation mark"):
+    pg.write('ENTER THE CONTENT/MESSAGE')
     pg.press('enter')
